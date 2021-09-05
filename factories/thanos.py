14c866adcfe5acaad6086caec7ff20ebc7b26060
lpereiras <you@example.com>
2021-09-05 14:06:40 -0300

    v2_automacao_cod_comentado

--- factories/thanos.py
+++ factories/thanos.py
@@ -1,9 +1,12 @@
+#Importa a biblioteca instalada via terminal 
 from faker import Faker
 faker = Faker()
 
+#Metodo que retorna um objeto com dados inseridos
 def factory_thanos():
 
     return {
+		#O .faker gera um nome randomico
         "name": faker.name(),
 	    "aliases": "Thanos",
 	    "age": 3000,
